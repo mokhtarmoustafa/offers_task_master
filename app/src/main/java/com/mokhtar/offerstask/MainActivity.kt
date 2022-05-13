5b2c4300ba1e05ddbc83cc9b0b2a2eca545d0ca3
mokhtarmoustafa <mokhtar.rnd@gmail.com>
2022-05-13 18:20:58 +0200

    Design -  intro -  click link to manager and employee view

--- app/src/main/java/com/mokhtar/offerstask/MainActivity.kt
+++ app/src/main/java/com/mokhtar/offerstask/MainActivity.kt
@@ -5,10 +5,15 @@ import android.os.Bundle
 import com.mokhtar.offerstask.util.Utility.setTransparentStatusBar
 
 class MainActivity : AppCompatActivity() {
+    //region variables
+
+    //region events
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTransparentStatusBar()
         setContentView(R.layout.activity_main)
 
     }
+
+    //endregion
 }
--- app/src/main/java/com/mokhtar/offerstask/ui/intro/IntroFragment.kt
+++ app/src/main/java/com/mokhtar/offerstask/ui/intro/IntroFragment.kt
@@ -5,13 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.mokhtar.offerstask.R
 import com.mokhtar.offerstask.databinding.FragmentIntroBinding
 
 class IntroFragment : Fragment() {
 
     //region variables
-    private  var _binding:FragmentIntroBinding? = null
+    private var _binding: FragmentIntroBinding? = null
     private val binding get() = _binding!!
     //endregion
 
@@ -22,11 +23,14 @@ class IntroFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        _binding=FragmentIntroBinding.inflate(layoutInflater,container,false)
+        _binding = FragmentIntroBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        init()
+    }
 
     override fun onDestroy() {
         super.onDestroy()
@@ -35,6 +39,18 @@ class IntroFragment : Fragment() {
     //endregion
 
     //region helper functions
+    private fun init() {
+        binding.ivEmployee.setOnClickListener { navigateToEmployeeMode() }
+        binding.ivManager.setOnClickListener { navigateToManagerMode() }
+    }
+
+    private fun navigateToEmployeeMode() {
+        findNavController().navigate(IntroFragmentDirections.actionIntroFragmentToEmployeeOfferFragment())
+    }
+
+    private fun navigateToManagerMode() {
+        findNavController().navigate(IntroFragmentDirections.actionIntroFragmentToManagerOfferFragment())
+    }
 
     //endregion
     companion object {
