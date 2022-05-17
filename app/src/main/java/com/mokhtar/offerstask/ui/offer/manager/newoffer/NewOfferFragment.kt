8bcdca279049ac09c487bb0dc48d199996de204c
mokhtarmoustafa <mokhtar.rnd@gmail.com>
2022-05-17 11:36:59 +0200

    design - adjust new offer screen

--- app/src/main/java/com/mokhtar/offerstask/ui/offer/manager/newoffer/NewOfferFragment.kt
+++ app/src/main/java/com/mokhtar/offerstask/ui/offer/manager/newoffer/NewOfferFragment.kt
@@ -6,8 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.mokhtar.offerstask.R
+import com.mokhtar.offerstask.data.Offer
 import com.mokhtar.offerstask.databinding.NewOfferFragmentBinding
 
 class NewOfferFragment : Fragment() {
@@ -22,6 +24,7 @@ class NewOfferFragment : Fragment() {
     }
 
 
+    //region events
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -33,11 +36,63 @@ class NewOfferFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.ivBack.setOnClickListener { findNavController().navigateUp() }
+
+        binding.btnCreate.setOnClickListener {
+            if(validateALl())
+                createNewOffer()
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
+
+    //endregion
+
+
+    //region helper functions
+    private fun validateALl(): Boolean {
+        if (binding.etOfferTitleData.text.toString().isNullOrEmpty()) {
+            Toast.makeText(requireContext(), "Title can't be nul or empty.", Toast.LENGTH_SHORT)
+                .show()
+            binding.etOfferTitleData.requestFocus()
+            return false
+        }
+        else if (binding.etOfferOrganizationData.text.toString().isNullOrEmpty()) {
+            Toast.makeText(requireContext(), "Organizations Data can't be nul or empty.", Toast.LENGTH_SHORT)
+                .show()
+            binding.etOfferOrganizationData.requestFocus()
+            return false
+        }
+        else if (binding.etOfferRuleData.text.toString().isNullOrEmpty()) {
+            Toast.makeText(requireContext(), "Offer Rule data can't be nul or empty.", Toast.LENGTH_SHORT)
+                .show()
+            binding.etOfferRuleData.requestFocus()
+            return false
+        }
+        else if (binding.etOfferBonusData.text.toString().isNullOrEmpty()) {
+            Toast.makeText(requireContext(), "Offer Bonus can't be nul or empty.", Toast.LENGTH_SHORT)
+                .show()
+            binding.etOfferBonusData.requestFocus()
+            return false
+        }
+         else if (binding.etOfferBenefitData.text.toString().isNullOrEmpty()) {
+            Toast.makeText(requireContext(), "Offer Benefits can't be nul or empty.", Toast.LENGTH_SHORT)
+                .show()
+            binding.etOfferBenefitData.requestFocus()
+            return false
+        }
+
+        return true
+    }
+
+
+    private fun createNewOffer()
+    {
+
+    }
+    //endregion
 
 }
